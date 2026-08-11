1ea94c42004799903d7f1881c4f835ffa692bf0b
Otto Eric <70889383+Otto200@users.noreply.github.com>
2026-08-11 16:49:41 +1000

    Update request-access.js

--- js/request-access.js
+++ js/request-access.js
@@ -1,4 +1,4 @@
-<script>
+
 function redirectToWhatsApp() {
   // Get the value typed by the user
   const userName = document.getElementById('ic-name-input').value.trim();
@@ -9,16 +9,16 @@ function redirectToWhatsApp() {
   }
   
   // REPLACE 67570000000 WITH YOUR ACTUAL WHATSAPP NUMBER WITH COUNTRY CODE (675 for PNG)
-  const whatsappNumber = "67570000000"; 
+  const whatsappNumber = "67576766296"; 
   
   // Create your custom URL-encoded pre-filled text message
   const messageText = `Hi Otto, I have completed the IC Markets setup. Please send me access to BANKBUGS|FX ecosystem. My registered full name for verification is: ${userName}`;
   const encodedMessage = encodeURIComponent(messageText);
   
   // Construct the final API URL
-  const whatsappUrl = `https://wa.me67576766296?text=${encodedMessage}`;
+  const whatsappUrl = `https://wa.me/67576766296?text=${encodedMessage}`;
   
   // Open WhatsApp in a new tab
   window.open(whatsappUrl, '_blank');
 }
-</script>
+
